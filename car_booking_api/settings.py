50954bea7a020be6cc62dd5a3543e1043b94bbfb
Jacobokia <cobs399@gmail.com>
2021-05-14 12:58:54 +0300

    add the sqllite database configuration

--- car_booking_api/settings.py
+++ car_booking_api/settings.py
@@ -81,19 +81,19 @@ WSGI_APPLICATION = 'car_booking_api.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'cba',
-        'USER': 'timothy',
-        'PASSWORD': os.getenv('PASSWORD'),
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+    #'default': {
+     #   'ENGINE': 'django.db.backends.postgresql_psycopg2',
+      #  'NAME': 'cba',
+      #  'USER': 'timothy',
+       # 'PASSWORD': os.getenv('PASSWORD'),
+       # 'HOST': '127.0.0.1',
+       # 'PORT': '5432',
 
-    }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+   # }
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+     }
 }
 
 
