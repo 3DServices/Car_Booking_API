fe6defaf70b8997953a82805b55201fee17ab87a
masikotimo <masikotimo@gmail.com>
2021-05-14 10:32:14 +0300

    test deletion in model tests

--- api/tests/modalTests.py
+++ api/tests/modalTests.py
@@ -5,32 +5,29 @@ import uuid
 
 
 class TestUserModal(TestCase):
-
-    def test_existence_of_user_created(self):
+    def setUp(self):
         user = User.objects.create(
             first_name='timo', last_name='timo', password='xsxsee2323')
+
+    def test_existence_of_user_created(self):
 
         check_existence = User.objects.filter(first_name='timo').exists()
         self.assertEqual(check_existence, True)
 
     def test_update_of_user_record(self):
-        user = User.objects.create(
-            first_name='timo', last_name='timo', password='xsxsee2323')
+
         user_store = User.objects.get(first_name='timo')
         user_store.first_name = 'masiko'
-
         user_store.save()
-
         check_existence = User.objects.filter(first_name='masiko').exists()
-        self.assertEqual(check_existence, True)
+        self.assertTrue(check_existence)
 
     def test_delete_of_user_record(self):
-        user = User.objects.create(
-            first_name='timo', last_name='timo', password='xsxsee2323')
+
         user_store = User.objects.get(first_name='timo')
         user_store.delete()
         check_existence = User.objects.filter(first_name='timo').exists()
-        self.assertEqual(check_existence, False)
+        self.assertFalse(check_existence)
 
 
 # guidance with the crud operation
