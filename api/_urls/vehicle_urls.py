caaad9bb453fd741c45f80b8e76bbde771658cb4
senjack <senjackglobal@gmail.com>
2021-05-20 16:12:03 +0300

    Fix bug : Vehicle Component not working
    Fix bug : Vehicle Component not working
    -----------------------------------------
    + Update Vehicle Model (Let it Inherit from BaseModel Mixin.)
    + Update Vehicle Serializer (Let it inherit from HyperlinkedModelSerializer)
    +Update Views : Decouple Vehicle views from Generic Views
    +Update URLs : Decouple Vehicle URLs from Generic URLs
    + Create endpoints for all CRUD operations on Vehicle.

--- api/_urls/vehicle_urls.py
+++ api/_urls/vehicle_urls.py
@@ -1,16 +1,9 @@
-from django.conf.urls import url, include
-from rest_framework import routers
-import api.views as views
+import api._views.vehicle_views as views
+from api.views import vehicle_views as views
 from django.urls import path
 
-
-router = routers.DefaultRouter()
-# router.register(r'vehicles/create', views.CreateVehicleViewSet,basename="create_vehicle")
-# # router.register(r'vehicles/create', views.CreateVehicleViewSet)
-# router.register(r'vehicles', views.ViewVehiclesListViewSet,basename="view_vehicles")
-# router.register(r'vehicles/<str:id>', views.RetrieveVehicleViewSet,basename="retrieve_vehicle")
-# router.register(r'vehicles/<str:id>/update', views.UpdateVehicleViewSet,basename="update_vehicle")
-# router.register(r'vehicles/<str:id>/delete', views.DeleteVehicleViewSet,basename="delete_vehicle")
+# from rest_framework import routers
+# router = routers.DefaultRouter()
 
 urlpatterns = [
    # path('', include(router.urls)),
@@ -18,6 +11,5 @@ urlpatterns = [
     path('', views.ViewVehiclesListViewSet.as_view({'get': 'list'}),name="view_vehicles"),
     path(r'<str:id>/', views.RetrieveVehicleViewSet.as_view({'get': 'retrieve'}),name="retrieve_vehicle"),
     path(r'<str:id>/update/', views.UpdateVehicleViewSet.as_view({'put': 'update'})),
-    path(r'<str:id>/delete/', views.DeleteVehicleViewSet.as_view({'destroy': 'destroy'})),
-    
+    path(r'<str:id>/delete/', views.DeleteVehicleViewSet.as_view({'delete': 'destroy'})),    
 ]
