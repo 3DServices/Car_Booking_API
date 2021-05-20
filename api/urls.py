8e732cb412d5660b57e291e6ad7882871c997542
senjack <senjackglobal@gmail.com>
2021-05-20 16:12:03 +0300

    Fix bug : Vehicle Component not working
    Fix bug : Vehicle Component not working
    -----------------------------------------
    + Update Vehicle Model (Let it Inherit from BaseModel Mixin.)
    + Update Vehicle Serializer (Let it inherit from HyperlinkedModelSerializer)
    +Update Views : Decouple Vehicle views from Generic Views
    +Update URLs : Decouple Vehicle URLs from Generic URLs
    + Create endpoints for all CRUD operations on Vehicle.

--- api/urls.py
+++ api/urls.py
@@ -1,28 +1,12 @@
-from django.conf.urls import url, include
-from django.contrib import admin
-from rest_framework import routers
-import api.views as views
+from django.conf.urls import include
 from django.urls import path
+from rest_framework import routers
 import api._urls.vehicle_urls as vehicle_urls
 
 
 router = routers.DefaultRouter()
 
-
-# router.register(r'vehicles/create', views.CreateVehicleViewSet,basename="create_vehicle")
-# # router.register(r'vehicles/create', views.CreateVehicleViewSet)
-# router.register(r'vehicles', views.ViewVehiclesListViewSet,basename="view_vehicles")
-# router.register(r'vehicles/<str:id>', views.RetrieveVehicleViewSet,basename="retrieve_vehicle")
-# router.register(r'vehicles/<str:id>/update', views.UpdateVehicleViewSet,basename="update_vehicle")
-# router.register(r'vehicles/<str:id>/delete', views.DeleteVehicleViewSet,basename="delete_vehicle")
-
 urlpatterns = [
     path('', include(router.urls)),
-    path(r'vehicles/', include('api._urls.vehicle_urls')),
-    # path(r'vehicles/create', views.CreateVehicleViewSet.as_view({'post': 'create'})),
-    # path(r'vehicles', views.ViewVehiclesListViewSet.as_view({'get': 'list'})),
-    # path(r'vehicles/<str:id>', views.RetrieveVehicleViewSet.as_view({'get': 'retrieve'})),
-    # path(r'vehicles/<str:id>/update', views.UpdateVehicleViewSet.as_view({'put': 'update'})),
-    # path(r'vehicles/<str:id>/delete', views.DeleteVehicleViewSet.as_view({'destroy': 'destroy'})),
-    
+    path(r'vehicles/', include(vehicle_urls)),
 ]
