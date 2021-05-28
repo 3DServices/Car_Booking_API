75b2320d7a3bdafd1745d9c6f3adf007fa94011f
masikotimo <masikotimo@gmail.com>
2021-05-28 17:31:31 +0300

    added passenger blacklist tests

--- api/tests/model_tests.py
+++ api/tests/model_tests.py
@@ -67,42 +67,6 @@ class TestOrganisationModal(TestCase):
         self.assertFalse(check_existence)
 
 
-class TestProjectModal(TestCase):
-    def setUp(self):
-        organisation = api_models.Organisation.objects.create(
-            name='Unra')
-
-        project = api_models.Project.objects.create(
-            name='projectx', organisation=organisation)
-
-    def test_existence_of_project_created(self):
-
-        check_existence = api_models.Project.objects.filter(
-            name='projectx').exists()
-        self.assertEqual(check_existence, True)
-
-    def test_update_of_project_record(self):
-
-        project_store = api_models.Project.objects.get(
-            name='projectx')
-        project_store.name = 'kakira'
-        project_store.save()
-        check_existence = api_models.Project.objects.filter(
-            name='kakira').exists()
-        self.assertTrue(check_existence)
-
-    def test_delete_of_project_record(self):
-
-        project_store = api_models.Project.objects.get(
-            name='projectx')
-        project_store.delete()
-        check_existence = api_models.Project.objects.filter(
-            name='projectx').exists()
-        self.assertFalse(check_existence)
-
-
-# organisationfleetmanager
-
 class TestOrganisationFleetManagerModal(TestCase):
 
     @classmethod
@@ -141,4 +105,256 @@ class TestOrganisationFleetManagerModal(TestCase):
         organisationfleetmanager.delete()
         check_existence = api_models.OrganisationFleetManager.objects.filter(
             organisation=self.organisation).exists()
+        self.assertFalse(check_existence)
+
+
+class TestProjectModal(TestCase):
+    def setUp(self):
+        organisation = api_models.Organisation.objects.create(
+            name='Unra')
+
+        project = api_models.Project.objects.create(
+            name='projectx', organisation=organisation)
+
+    def test_existence_of_project_created(self):
+
+        check_existence = api_models.Project.objects.filter(
+            name='projectx').exists()
+        self.assertEqual(check_existence, True)
+
+    def test_update_of_project_record(self):
+
+        project_store = api_models.Project.objects.get(
+            name='projectx')
+        project_store.name = 'kakira'
+        project_store.save()
+        check_existence = api_models.Project.objects.filter(
+            name='kakira').exists()
+        self.assertTrue(check_existence)
+
+    def test_delete_of_project_record(self):
+
+        project_store = api_models.Project.objects.get(
+            name='projectx')
+        project_store.delete()
+        check_existence = api_models.Project.objects.filter(
+            name='projectx').exists()
+        self.assertFalse(check_existence)
+
+
+class TestStationModal(TestCase):
+    def setUp(self):
+        organisation = api_models.Organisation.objects.create(
+            name='Unra')
+
+        station = api_models.Station.objects.create(
+            name='Stationx', organisation=organisation)
+
+    def test_existence_of_station_created(self):
+
+        check_existence = api_models.Station.objects.filter(
+            name='Stationx').exists()
+        self.assertEqual(check_existence, True)
+
+    def test_update_of_station_record(self):
+
+        station_store = api_models.Station.objects.get(
+            name='Stationx')
+        station_store.name = 'kakira'
+        station_store.save()
+        check_existence = api_models.Station.objects.filter(
+            name='kakira').exists()
+        self.assertTrue(check_existence)
+
+    def test_delete_of_station_record(self):
+
+        station_store = api_models.Station.objects.get(
+            name='Stationx')
+        station_store.delete()
+        check_existence = api_models.Station.objects.filter(
+            name='Stationx').exists()
+        self.assertFalse(check_existence)
+
+
+class TestBranchModal(TestCase):
+    def setUp(self):
+        organisation = api_models.Organisation.objects.create(
+            name='Unra')
+
+        branch = api_models.Branch.objects.create(
+            name='Branchx', organisation=organisation)
+
+    def test_existence_of_branch_created(self):
+
+        check_existence = api_models.Branch.objects.filter(
+            name='Branchx').exists()
+        self.assertEqual(check_existence, True)
+
+    def test_update_of_branch_record(self):
+
+        branch_store = api_models.Branch.objects.get(
+            name='Branchx')
+        branch_store.name = 'kakira'
+        branch_store.save()
+        check_existence = api_models.Branch.objects.filter(
+            name='kakira').exists()
+        self.assertTrue(check_existence)
+
+    def test_delete_of_branch_record(self):
+
+        branch_store = api_models.Branch.objects.get(
+            name='Branchx')
+        branch_store.delete()
+        check_existence = api_models.Branch.objects.filter(
+            name='Branchx').exists()
+        self.assertFalse(check_existence)
+
+
+class TestDepartmentModal(TestCase):
+    def setUp(self):
+        organisation = api_models.Organisation.objects.create(
+            name='Unra')
+
+        department = api_models.Department.objects.create(
+            name='Departmentx', organisation=organisation)
+
+    def test_existence_of_department_created(self):
+
+        check_existence = api_models.Department.objects.filter(
+            name='Departmentx').exists()
+        self.assertEqual(check_existence, True)
+
+    def test_update_of_department_record(self):
+
+        department_store = api_models.Department.objects.get(
+            name='Departmentx')
+        department_store.name = 'kakira'
+        department_store.save()
+        check_existence = api_models.Department.objects.filter(
+            name='kakira').exists()
+        self.assertTrue(check_existence)
+
+    def test_delete_of_department_record(self):
+
+        department_store = api_models.Department.objects.get(
+            name='Departmentx')
+        department_store.delete()
+        check_existence = api_models.Department.objects.filter(
+            name='Departmentx').exists()
+        self.assertFalse(check_existence)
+
+
+class TestDirectorateModal(TestCase):
+    def setUp(self):
+        organisation = api_models.Organisation.objects.create(
+            name='Unra')
+
+        directorate = api_models.Directorate.objects.create(
+            name='Directoratex', organisation=organisation)
+
+    def test_existence_of_directorate_created(self):
+
+        check_existence = api_models.Directorate.objects.filter(
+            name='Directoratex').exists()
+        self.assertEqual(check_existence, True)
+
+    def test_update_of_directorate_record(self):
+
+        directorate_store = api_models.Directorate.objects.get(
+            name='Directoratex')
+        directorate_store.name = 'kakira'
+        directorate_store.save()
+        check_existence = api_models.Directorate.objects.filter(
+            name='kakira').exists()
+        self.assertTrue(check_existence)
+
+    def test_delete_of_directorate_record(self):
+
+        directorate_store = api_models.Directorate.objects.get(
+            name='Directoratex')
+        directorate_store.delete()
+        check_existence = api_models.Directorate.objects.filter(
+            name='Directoratex').exists()
+        self.assertFalse(check_existence)
+
+
+class TestBlacklistModal(TestCase):
+    def setUp(cls):
+        cls.organisation = api_models.Organisation.objects.create(
+            name='Unra')
+
+        cls.organisation1 = api_models.Organisation.objects.create(
+            name='Unrax')
+
+        cls.blacklist = api_models.Blacklist.objects.create(
+            organisation=cls.organisation)
+
+    def test_existence_of_blacklist_created(self):
+
+        check_existence = api_models.Blacklist.objects.filter(
+            organisation=self.organisation).exists()
+        self.assertEqual(check_existence, True)
+
+    def test_update_of_blacklist_record(self):
+
+        blacklist_store = api_models.Blacklist.objects.get(
+            organisation=self.organisation)
+        blacklist_store.organisation = self.organisation1
+        blacklist_store.save()
+        check_existence = api_models.Blacklist.objects.filter(
+            organisation=self.organisation1).exists()
+        self.assertTrue(check_existence)
+
+    def test_delete_of_blacklist_record(self):
+
+        blacklist_store = api_models.Blacklist.objects.get(
+            organisation=self.organisation)
+        blacklist_store.delete()
+        check_existence = api_models.Blacklist.objects.filter(
+            organisation=self.organisation).exists()
+        self.assertFalse(check_existence)
+
+
+class TestPassengerBlacklistModal(TestCase):
+    def setUp(cls):
+        cls.organisation = api_models.Organisation.objects.create(
+            name='Unrax')
+
+        cls.blacklist = api_models.Blacklist.objects.create(
+            organisation=cls.organisation)
+        cls.blacklist1 = api_models.Blacklist.objects.create(
+            organisation=cls.organisation)
+
+        cls.user = auth_models.User.objects.create(
+            first_name='timo', last_name='timo', password='xsxsee2323')
+
+        cls.passenger = auth_models.Passenger.objects.create(
+            user=cls.user)
+
+        cls.passengerblacklist = api_models.PassengerBlacklist.objects.create(
+            blacklist=cls.blacklist, passenger=cls.passenger)
+
+    def test_existence_of_passengerblacklist_created(self):
+
+        check_existence = api_models.PassengerBlacklist.objects.filter(
+            blacklist=self.blacklist).exists()
+        self.assertEqual(check_existence, True)
+
+    def test_update_of_passengerblacklist_record(self):
+
+        passengerblacklist = api_models.PassengerBlacklist.objects.get(
+            blacklist=self.blacklist)
+        passengerblacklist.blacklist = self.blacklist1
+        passengerblacklist.save()
+        check_existence = api_models.PassengerBlacklist.objects.filter(
+            blacklist=self.blacklist1).exists()
+        self.assertTrue(check_existence)
+
+    def test_delete_of_passengerblacklist_record(self):
+
+        passengerblacklist = api_models.PassengerBlacklist.objects.get(
+            blacklist=self.blacklist)
+        passengerblacklist.delete()
+        check_existence = api_models.PassengerBlacklist.objects.filter(
+            blacklist=self.blacklist).exists()
         self.assertFalse(check_existence)
