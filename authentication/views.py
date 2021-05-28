e7a83b19c9651200c5b003aa3e4a47623168c4aa
masikotimo <masikotimo@gmail.com>
2021-05-28 10:08:12 +0300

    worked on serializers for authentication

--- authentication/views.py
+++ authentication/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from authentication.models import User
 from rest_framework import viewsets
 from authentication.serializers import UserSerializer
+import authentication._views.system_admin as system_admin_views
+import authentication._views.driver as driver_views
+import authentication._views.passenger as passenger_views
+import authentication._views.fleet_manager as fleet_manager_views
 
 
 # Create your views here.
