2438efdc5e767488d8672b269eb2631dff185f62
masikotimo <masikotimo@gmail.com>
2021-06-01 15:39:33 +0300

    engage friendly errors with drf friendly error

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,33 +1,34 @@
 from authentication.models import User, SystemAdmin, Driver, Passenger, FleetManager
 from rest_framework import serializers
+from rest_framework_friendly_errors.mixins import FriendlyErrorMessagesMixin
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer, FriendlyErrorMessagesMixin):
     class Meta:
         model = User
         fields = '__all__'
         lookup_field = 'id'
 
 
-class SystemAdminSerializer(serializers.HyperlinkedModelSerializer):
+class SystemAdminSerializer(serializers.ModelSerializer, FriendlyErrorMessagesMixin):
     class Meta:
         model = SystemAdmin
         fields = '__all__'
 
 
-class DriverSerializer(serializers.HyperlinkedModelSerializer):
+class DriverSerializer(serializers.ModelSerializer, FriendlyErrorMessagesMixin):
     class Meta:
         model = Driver
         fields = '__all__'
 
 
-class PassengerSerializer(serializers.HyperlinkedModelSerializer):
+class PassengerSerializer(serializers.ModelSerializer, FriendlyErrorMessagesMixin):
     class Meta:
         model = Passenger
         fields = '__all__'
 
 
-class FleetManagerSerializer(serializers.HyperlinkedModelSerializer):
+class FleetManagerSerializer(serializers.ModelSerializer, FriendlyErrorMessagesMixin):
     class Meta:
         model = FleetManager
         fields = '__all__'
