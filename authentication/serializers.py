f2935f1d7931da2b72d9fb8c1246cbe348d3c478
Jacobokia <cobs399@gmail.com>
2021-06-02 15:01:02 +0300

    change the strings for the model and serializers depth

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -14,21 +14,24 @@ class SystemAdminSerializer(serializers.ModelSerializer, FriendlyErrorMessagesMi
     class Meta:
         model = SystemAdmin
         fields = '__all__'
-
+        depth = 1
 
 class DriverSerializer(serializers.ModelSerializer, FriendlyErrorMessagesMixin):
     class Meta:
         model = Driver
         fields = '__all__'
+        depth = 1
 
 
 class PassengerSerializer(serializers.ModelSerializer, FriendlyErrorMessagesMixin):
     class Meta:
         model = Passenger
         fields = '__all__'
+        depth = 1
 
 
 class FleetManagerSerializer(serializers.ModelSerializer, FriendlyErrorMessagesMixin):
     class Meta:
         model = FleetManager
         fields = '__all__'
+        depth = 1
