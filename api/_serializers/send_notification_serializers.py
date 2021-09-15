e38869aec7334c2e77b6beae2bba2b0c59c1fb0a
masikotimo <masikotimo@gmail.com>
2021-09-15 13:05:09 +0300

    work on notifications to show which trip has been approved and reflect the username

--- api/_serializers/send_notification_serializers.py
+++ api/_serializers/send_notification_serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 from business_logic.utilities.expo_notification import send_push_message
+from authentication.models import *
+from api.models import *
+from core.utilities.rest_exceptions import (ValidationError)
 
 
 class SendNotification:
@@ -8,8 +11,25 @@ class SendNotification:
 
 
 class SendNotificationSerializer(serializers.Serializer):
-    token = serializers.CharField(required=True, write_only=True)
+    passenger = serializers.UUIDField(required=True, write_only=True)
+    trip = serializers.UUIDField(required=True, write_only=True)
 
     def create(self, validated_data):
-        token = validated_data.pop('token')
-        return SendNotification(token)
+        passenger = validated_data.pop('passenger')
+
+        passenger_instances = Passenger.objects.all().filter(id=passenger)
+        if not passenger_instances.exists():
+            raise ValidationError({'passenger': 'Passenger doesnt exist !'})
+
+        passenger_notification_instances = PassengerNotification.objects.filter(
+            passenger=passenger_instances[0])
+
+        if not passenger_notification_instances.exists():
+            raise ValidationError(
+                {'passenger': 'Passenger has not yet regisered his application to receive notifications !'})
+
+        token = passenger_notification_instances[0].notification.expo_token
+
+        # send_push_message(token)
+
+        return token
