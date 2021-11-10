72d172b8c536526eef05ffe3f19a8a8272c0851a
masikotimo <masikotimo@gmail.com>
2021-11-10 19:07:09 +0300

    change driver and passenger serializers

--- api/_serializers/organisation_driver_serializers.py
+++ api/_serializers/organisation_driver_serializers.py
@@ -5,15 +5,29 @@ from authentication._serializers.driver_serializers import DriverSerializer
 from core.modules.rest_framework_modules import serializers
 from core.utilities.rest_exceptions import (ValidationError)
 from core.mixins.serializer_mixins import ModelSerializer
+from business_logic.system_users._user import User as UserFacade
 
 
 class CreateOrganisationDriverSerializer(ModelSerializer):
-    driver = serializers.UUIDField(required=True, write_only=True)
+    email = serializers.EmailField(
+        max_length=254,
+        min_length=5,
+        required=True,
+        write_only=True,
+    )
+    password = serializers.CharField(
+        max_length=254,
+        min_length=6,
+        required=True,
+        write_only=True,
+        help_text='Required',
+        style={'input_type': 'password', 'placeholder': 'Password'})
+
     organisation = serializers.UUIDField(required=True, write_only=True)
 
     class Meta:
         model = api_models.OrganisationDriver
-        fields = ['organisation', 'driver']
+        fields = ['organisation', 'email', 'password']
         lookup_field = 'id'
         depth = 2
         extra_kwargs = {
@@ -25,20 +39,21 @@ class CreateOrganisationDriverSerializer(ModelSerializer):
     def create(self, validated_data):
         _request = self.context['request']
         request = {'request': _request, 'validated_data': validated_data}
-        driver = validated_data.pop('driver', None)
+
         organisation = validated_data.pop('organisation', None)
-        driver_instances = auth_models.Driver.objects.all().filter(id=driver)
         organisation_instances = api_models.Organisation.objects.all().filter(id=organisation)
 
-        if not driver_instances.exists():
-            raise ValidationError({'driver': 'Invalid value!'})
         if not organisation_instances.exists():
             raise ValidationError({'organisation': 'Invalid value!'})
 
-        organisation_driver_instance = api_models.OrganisationDriver.objects.create(
-            driver=driver_instances[0], organisation=organisation_instances[0])
+        driver_registered = UserFacade().register_driver(request)
+        driver_instances = auth_models.Driver.objects.all()
+        for driver in driver_instances:
+            if(driver.user.email == validated_data["email"]):
+                organisation_driver_instance = api_models.OrganisationDriver.objects.create(
+                    driver=driver, organisation=organisation_instances[0])
 
-        return organisation_driver_instance
+                return driver_registered
 
 
 class OrganisationDriverSerializer(ModelSerializer):
--- api/_serializers/organisation_passenger_serializers.py
+++ api/_serializers/organisation_passenger_serializers.py
@@ -2,19 +2,33 @@ import api.models as api_models
 import authentication.models as auth_models
 from core.modules.rest_framework_modules import serializers
 from api._serializers.organisation_serializers import OrganisationSerializer
-from authentication._serializers.passenger_serializers import PassengerSerializer
+from authentication._serializers.passenger_serializers import CreatePassengerSerializer, PassengerSerializer
 from core.modules.rest_framework_modules import serializers
 from core.utilities.rest_exceptions import (ValidationError)
 from core.mixins.serializer_mixins import ModelSerializer
+from business_logic.system_users._user import User as UserFacade
 
 
 class CreateOrganisationPassengerSerializer(ModelSerializer):
-    passenger = serializers.UUIDField(required=True, write_only=True)
+    email = serializers.EmailField(
+        max_length=254,
+        min_length=5,
+        required=True,
+        write_only=True,
+    )
+    password = serializers.CharField(
+        max_length=254,
+        min_length=6,
+        required=True,
+        write_only=True,
+        help_text='Required',
+        style={'input_type': 'password', 'placeholder': 'Password'}
+    )
     organisation = serializers.UUIDField(required=True, write_only=True)
 
     class Meta:
         model = api_models.OrganisationPassenger
-        fields = ['organisation', 'passenger']
+        fields = ['email', 'password', 'organisation']
         lookup_field = 'id'
         depth = 2
         extra_kwargs = {
@@ -23,21 +37,22 @@ class CreateOrganisationPassengerSerializer(ModelSerializer):
 
     def create(self, validated_data):
         _request = self.context['request']
-        request = {'request': _request, 'validated_data': validated_data}
-        passenger = validated_data.pop('passenger', None)
         organisation = validated_data.pop('organisation', None)
-        passenger_instances = auth_models.Passenger.objects.all().filter(id=passenger)
+
         organisation_instances = api_models.Organisation.objects.all().filter(id=organisation)
 
-        if not passenger_instances.exists():
-            raise ValidationError({'passenger': 'Invalid value!'})
         if not organisation_instances.exists():
             raise ValidationError({'organisation': 'Invalid value!'})
 
-        organisation_passenger_instance = api_models.OrganisationPassenger.objects.create(
-            passenger=passenger_instances[0], organisation=organisation_instances[0])
+        request = {'request': _request, 'validated_data': validated_data}
+        passenger_registered = UserFacade().register_passenger(request)
+        passenger_instances = auth_models.Passenger.objects.all()
+        for passenger in passenger_instances:
+            if(passenger.user.email == validated_data["email"]):
+                organisation_passenger_instance = api_models.OrganisationPassenger.objects.create(
+                    passenger=passenger, organisation=organisation_instances[0])
 
-        return organisation_passenger_instance
+                return passenger_registered
 
 
 class OrganisationPassengerSerializer(ModelSerializer):
